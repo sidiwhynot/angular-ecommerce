6324e165853e777c587940f888bd7061b93bad6f
eljily <eljilysidibrahim@gmail.com>
2024-05-31 00:17:56 +0000

    hope it works

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,9 +1,10 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { appConfig } from './app.config';
 import { provideServerRendering } from './provide-server-rendering'; // Assurez-vous de corriger le chemin d'importation selon l'emplacement réel de votre fichier
+import { provideClientHydration } from '@angular/platform-browser';
 
 const serverConfig: ApplicationConfig = {
-  providers: [provideServerRendering()],
+  providers: [provideServerRendering(), provideClientHydration()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
