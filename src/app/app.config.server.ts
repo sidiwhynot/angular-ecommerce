9ecfbadd4dd8aa14d4a8b333ece6cc5829a0460d
eljily <eljilysidibrahim@gmail.com>
2024-05-31 00:03:20 +0000

    resolved warn

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -3,9 +3,7 @@ import { appConfig } from './app.config';
 import { provideServerRendering } from './provide-server-rendering'; // Assurez-vous de corriger le chemin d'importation selon l'emplacement réel de votre fichier
 
 const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering()
-  ]
+  providers: [provideServerRendering()],
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
